a508dc684e08fb5e8cbe993d81202ffd7c2107f0
sroyz <sroyz@cisco.com>
2018-04-21 11:25:37 +0200

    cleanup'

--- _site/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -x
-
-docker run --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll:3.5 jekyll build
--- _site/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -x
-
-docker run --rm --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:3.5 jekyll serve
